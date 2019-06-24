c74cf00c3b0f1e3e8f26c06699f2868eabcb1814
carneirofc <claudiofcarneiro@hotmail.com>
2019-06-24 17:04:06 -0300

    MotorDriverGPIO interface

---
+++ MotorDriverGPIO/driver.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import Adafruit_BBIO.GPIO as GPIO
+import converters
+import threading
+import time
+
+# Pino de comando dos dois relês usados para habilitar/desabilitar os drivers
+
+DRIVER_ENABLE_PIN = "P9_14"
+
+# Inicializa o pino de comando dos relês, desabilitando em seguida os drivers
+
+GPIO.setup(DRIVER_ENABLE_PIN,  GPIO.OUT)
+GPIO.output(DRIVER_ENABLE_PIN, GPIO.HIGH)
+
+class RF_MotorControllers_Driver():
+
+    def __init__(self):
+        self.ADC1 = converters.ADC()
+        self.ADC2 = converters.ADC2()
+
+    def data(self):
+        ADC1_values = [0] * 8
+        ADC2_values = [0] * 8
+
+        for i in range(10):
+            ADC1_readings = self.ADC1.read(range(8))
+            ADC2_readings = self.ADC2.read(range(8))
+            for j in range(8):
+                ADC1_values[j] += ADC1_readings[j]
+                ADC2_values[j] += ADC2_readings[j]
+
+        for i in range(8):
+            ADC1_values[i] = ((ADC1_values[i] / 10.0) / 4095.0) * 5.0
+            ADC2_values[i] = ((ADC2_values[i] / 10.0) / 4095.0) * 5.0
+
+        return str(ADC1_values + ADC2_values)
+
+    def drvEnbl(self, value):
+        if (value == 0):
+            GPIO.output(DRIVER_ENABLE_PIN, GPIO.LOW)
+        else:
+            GPIO.output(DRIVER_ENABLE_PIN, GPIO.HIGH)
+
+        return True
---
+++ MotorDriverGPIO/server.py
@@ -0,0 +1,69 @@
+import logging
+import os
+import socket
+import re
+
+from driver import RF_MotorControllers_Driver
+logger = logging.getLogger()
+
+class Comm():
+    def __init__(self, unix_socket_path, *args, **kwargs):
+        self.unix_socket_path = unix_socket_path
+        self.connection = None
+        self.welcome_socket = None
+        self.motor = RF_MotorControllers_Driver()
+
+    def serve(self):
+        try:
+            if os.path.exists(self.unix_socket_path):
+                logger.warning('Unix socket {} already exist'.format(self.unix_socket_path))
+                os.unlink(self.unix_socket_path)
+
+            if self.welcome_socket != None:
+                logger.warning('Welcome socket already istantiated')
+
+            self.welcome_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            self.welcome_socket.bind(self.unix_socket_path)
+
+            logger.info('Unix socket created at {}'.format(self.unix_socket_path))
+            self.welcome_socket.listen(1)
+
+            while True:
+                logger.info('Unix welcome socket listening')
+                connection, client_address = self.welcome_socket.accept()
+                logger.info('Client {} connected'.format(client_address))
+
+                connection.settimeout(30)
+
+                self.handle_connection(connection)
+        except:
+            logger.exception('Comm exception')
+        finally:
+            self.welcome_socket.close()
+            os.remove(self.unix_socket_path)
+            logger.info('Comm server shutdown')
+            self.welcome_socket = None
+
+    def handle_connection(self, connection):
+        try:
+            while True:
+                command = connection.recv(1024).decode('utf-8')
+                response = ResponseType.NO_RESPONSE
+
+                if command == 'DATA?':
+                    response = 'DATA ' + self.motor.read()
+                else :
+                    try:
+                        match = re.search(r'DRV_ENBL (0|1)', command)
+                        if hasattr(match, 'group'):
+                            response = self.motor.drvEnbl(int(match.group(1)))
+                    except:
+                        logger.exception('Wrong command')
+
+                connection.sendall('{}\r\n'.format(response).encode('utf-8'))
+                logger.debug('Command {} Length {}'.format(command, response))
+        except:
+            logger.exception('Connection exception')
+        finally:
+            logger.warning('Connection closed')
+            connection.close()
