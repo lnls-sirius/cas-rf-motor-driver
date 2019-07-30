8368e0b321883cb3bf82ee7e63003b49f9ac0ac3
carneirofc <claudiofcarneiro@hotmail.com>
2019-07-30 09:32:20 -0300

    Added a 30 sec delay

--- MotorDriverGPIO/run.py
+++ MotorDriverGPIO/run.py
@@ -2,6 +2,7 @@
 import argparse
 import logging
 import os
+import time
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='RF motor drivers GPIO',
@@ -10,21 +11,21 @@ if __name__ == '__main__':
     parser.add_argument('--debug', action='store_true')
     args = parser.parse_args()
 
-    os.system('config-pin P9_17 spi_cs')         # CS
-    os.system('config-pin P9_21 spi')            # DO
-    os.system('config-pin P9_18 spi')            # DI
-    os.system('config-pin P9_22 spi_sclk')       # CLK
-
-    os.system('config-pin P9_14 out')            # Relay
-
-    os.system('config-pin P9_24 out')            # ADC 1
-    os.system('config-pin P9_26 out')            # ADC2
-
     logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO, format='%(asctime)-15s [%(levelname)s] %(message)s',
         datefmt='%d/%m/%Y %H:%M:%S')
     logger = logging.getLogger()
 
-    from server import Comm
+    logger.info("Sleep for 30 seconds ...")
+    time.sleep(30)
 
+    os.system('config-pin P9_17 spi_cs')    # CS
+    os.system('config-pin P9_21 spi')       # DO
+    os.system('config-pin P9_18 spi')       # DI
+    os.system('config-pin P9_22 spi_sclk')  # CLK
+    os.system('config-pin P9_14 out')       # Relay
+    os.system('config-pin P9_24 out')       # ADC 1
+    os.system('config-pin P9_26 out')       # ADC2
+
+    from server import Comm
     comm = Comm(args.unix_socket_path)
     comm.serve()
